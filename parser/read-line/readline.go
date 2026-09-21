064ec72b221db70578268cf1a187ac332758d1f3
Moein Hoseini <moein.devht@gmail.com>
2026-09-21 15:25:12 +0330

    add readline package for sprating lines from buffer

---
+++ parser/read-line/readline.go
@@ -0,0 +1,42 @@
+package readLine
+
+import (
+	"bytes"
+	"io"
+)
+
+type LineReader struct {
+	reader io.Reader
+	buffer []byte
+}
+
+func NewLineReader(reader io.Reader) *LineReader {
+	return &LineReader{
+		reader: reader,
+		buffer: make([]byte, 0),
+	}
+}
+
+func (lr *LineReader) Read() (string, error) {
+	for {
+		if index := bytes.IndexByte(lr.buffer, '\n'); index != -1 {
+			line := string(lr.buffer[:index])
+
+			lr.buffer = lr.buffer[index+1:]
+
+			return line, nil
+		}
+
+		readBuffer := make([]byte, 1024)
+
+		n, err := lr.reader.Read(readBuffer)
+
+		if n > 0 {
+			lr.buffer = append(lr.buffer, readBuffer[:n]...)
+		}
+
+		if err != nil {
+			return "", err
+		}
+	}
+}
